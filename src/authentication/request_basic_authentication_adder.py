e97c23f8caa81e9b346a2740601c549179d23d4f
Daniel Nozka <danozka@gmail.com>
2025-04-02 14:40:19 +0200

    Adds missing type

--- src/authentication/request_basic_authentication_adder.py
+++ src/authentication/request_basic_authentication_adder.py
@@ -12,6 +12,6 @@ class RequestBasicAuthenticationAdder(IRequestAuthenticationAdder):
     def __init__(self, user: str, password: str) -> None:
         self._encoded_credentials = base64.b64encode(f'{user}:{password}'.encode()).decode()
 
-    def add_authentication_to_request(self, request: Request):
+    def add_authentication_to_request(self, request: Request) -> None:
         self._log.debug(f'[{request_id_context.get()}] Adding basic authentication headers...')
         request.headers['Proxy-Authorization'] = f'Basic {self._encoded_credentials}'
